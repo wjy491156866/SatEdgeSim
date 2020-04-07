43d7d2209ce15010924d188a91d63c1cd96b281f
wjy491156866 <491156866@qq.com>
2020-04-07 23:30:31 +0800

    update 2020.4.7  add RANDOM_VM algorithm

--- SatEdgeSim/edu/weijunyong/satedgesim/TasksOrchestration/DefaultEdgeOrchestrator.java
+++ SatEdgeSim/edu/weijunyong/satedgesim/TasksOrchestration/DefaultEdgeOrchestrator.java
@@ -29,6 +29,8 @@ public class DefaultEdgeOrchestrator extends Orchestrator {
 			return TradiPolling(architecture, task);
 		} else if ("WEIGHT_GREEDY".equals(algorithm)) {
 			return weightGreedy(architecture, task);
+		} else if ("RANDOM_VM".equals(algorithm)) {
+			return RandomVm(architecture, task);
 		} else {
 			SimLog.println("");
 			SimLog.println("Default Orchestrator- Unknnown orchestration algorithm '" + algorithm
@@ -211,85 +213,35 @@ public class DefaultEdgeOrchestrator extends Orchestrator {
 	}
 	
 	
-	
-	//***************************************************************************************************************
-	/*
-	
-	//根据贪心算法将一组任务分配给一组虚拟机，使总执行时间接近最短
-	public void bindCloudletsToVmsTimeAwared(){
-		int cloudletNum=cloudletList.size();
-		int vmNum=vmList.size();
-		//time[i][j] 表示任务i在虚拟机j上的执行时间
-		double[][] time=new double[cloudletNum][vmNum];
-		//cloudletList按MI降序排列, vm按MIPS升序排列
-		Collections.sort(cloudletList,new CloudletComparator());
-		Collections.sort(vmList,new VmComparator());
-
-		for(int i=0;i<cloudletNum;i++){
-			for(int j=0;j<vmNum;j++){
-				time[i][j]=
-					(double)cloudletList.get(i).getCloudletLength()/vmList.get(j).getMips();
-				//System.out.print("time["+i+"]["+j+"]="+time[i][j]+" ");
-				//For test
+	//随机一个vm
+	private int RandomVm(String[] architecture, Task task) {
+		List<Vm> vmList = simulationManager.getServersManager().getVmList();
+		int vm = -1;
+		int RandomCount = 0; // random time;
+		// get random vm for this task
+		while(RandomCount<orchestrationHistory.size()) {
+			double d = Math.random();
+			int index = (int)(d*(orchestrationHistory.size()-1));
+			if (offloadingIsPossible(task, vmList.get(index), architecture)) {
+				vm = index;
+				break;
 			}
-			//System.out.println(); //For test
+			RandomCount++;
 		}
-		double[] vmLoad=new double[vmNum];//在某个虚拟机上任务的总执行时间
-		int[] vmTasks=new int[vmNum]; //在某个Vm上运行的任务数量
-		double minLoad=0;//记录当前任务分配方式的最优值
-		int idx=0;//记录当前任务最优分配方式对应的虚拟机列号
-		//第一个cloudlet分配给最快的vm
-		vmLoad[vmNum-1]=time[0][vmNum-1];
-		vmTasks[vmNum-1]=1;
-		cloudletList.get(0).setVmId(vmList.get(vmNum-1).getId());
-		for(int i=1;i<cloudletNum;i++){
-			minLoad=vmLoad[vmNum-1]+time[i][vmNum-1];
-			idx=vmNum-1;
-			for(int j=vmNum-2;j>=0;j--){
-				//如果当前虚拟机未分配任务，则比较完当前任务
-				//分配给该虚拟机是否最优，即可以退出循环
-				if(vmLoad[j]==0){
-					if(minLoad>=time[i][j])
-						idx=j;
+		
+		//万一没有随机出来
+		if(RandomCount>=orchestrationHistory.size()) {
+			for (int i = 0; i < orchestrationHistory.size(); i++) {
+				if (offloadingIsPossible(task, vmList.get(i), architecture)) {
+					vm =i;
 					break;
 				}
-				if(minLoad>vmLoad[j]+time[i][j]){
-					minLoad=vmLoad[j]+time[i][j];
-					idx=j;
-				}
-				//简单的负载均衡
-				else if(minLoad==vmLoad[j]+time[i][j]&&vmTasks[j]<vmTasks[idx])
-					idx=j;
 			}
-			vmLoad[idx]+=time[i][idx];
-			vmTasks[idx]++;
-			cloudletList.get(i).setVmId(vmList.get(idx).getId());
-			//System.out.print(i+"th "+"vmLoad["+idx+"]="+vmLoad[idx]+"minLoad="+minLoad);
-			//System.out.println();
+			RandomCount = 0;
 		}
+		// assign the tasks to the found vm
+		return vm;
 	}
-	//根据指令长度降序排列任务，需要导入包Java.util.Comparator
-	private class CloudletComparator implements Comparator<Cloudlet>{
-		public int compare(Cloudlet cl1,Cloudlet cl2) {
-			return (int)(cl2.getCloudletLength()-cl1.getCloudletLength());
-		}
-	}
-	
-	//根据执行速度升序排列虚拟机
-		private class VmComparator implements Comparator<Vm>{
-			public int compare(Vm vm1,Vm vm2) {
-				return (int)(vm1.getMips()-vm2.getMips());
-			}
-		}
-	
-	*/
-	//***************************************************************************************************************
-
-	
-	
-	
-	
-	
 	
 
 	@Override
